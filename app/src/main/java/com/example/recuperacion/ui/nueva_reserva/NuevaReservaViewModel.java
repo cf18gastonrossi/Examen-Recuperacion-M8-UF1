b62e52c2d698c1131aa6adddd50984f34d7e6b3e
Gastón Rossi <gaston.rossi@basetis.com>
2020-06-08 16:35:03 +0200

    Adapting code

---
+++ app/src/main/java/com/example/recuperacion/ui/nueva_reserva/NuevaReservaViewModel.java
@@ -0,0 +1,7 @@
+package com.example.recuperacion.ui.nueva_reserva;
+
+import androidx.lifecycle.ViewModel;
+
+public class NuevaReservaViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
